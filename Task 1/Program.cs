146dec27620236951a151e48ed7212aaf548aa02
Ivica janevski <janevski.ivica@outlook.com>
2020-04-09 14:51:55 +0200

    Add project files.

---
+++ Task 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_1
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stats rezult = new Stats();
+            rezult.NumberStats();
+
+            string userImput = Console.ReadLine();
+
+
+       
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task 1/Stats.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1
+{
+  public class Stats
+    {
+
+        public void NumberStats()
+        {
+            Console.WriteLine("Enter Number");
+
+            double userImput = 0;
+            bool isDouble = double.TryParse(Console.ReadLine(),out userImput);
+
+            if (isDouble == true)
+            {
+                if (userImput < 0)
+                {
+                    Console.WriteLine("Negative");
+                }
+                else Console.WriteLine("Positive");
+
+                if (userImput % 2 == 0)
+                {
+                    Console.WriteLine("eaven");
+                }
+                else Console.WriteLine("odd");
+
+                if (userImput % 1 == 0)
+                {
+                    Console.WriteLine("not decimal");
+                }
+                else Console.WriteLine("decimal");
+
+            }
+            else Console.WriteLine("Eror...You chose poorly...Enter Number pls!");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
