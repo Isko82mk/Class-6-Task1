2afcfb4c30555498dc6e505e29074fdaa36762f5
Ivica janevski <janevski.ivica@outlook.com>
2020-04-09 15:43:17 +0200

    uppdate

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -6,7 +6,30 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Users fistUser = new Users("John Doe", "JDo@somthing.com", "abracadabra123",);
+            Users secUser = new Users("Simon Shoore", " LowSelfEsteem.com", "Adune_altar2519");
+            Users thirdUser = new Users("John Hop", "WhatDAF@shityWeb.com", " aida64_$ ");
+
+             Users[] Mesages =new Users[]{fistUser,secUser,thirdUser};
+
+            //foreach (Users user in Mesages)
+            //{
+            //    Console.WriteLine(user.Password);
+            //}
+
+            Console.WriteLine("Plesea log in\n" +
+                "Enter user name");
+            string usernameImput = Console.ReadLine();
+            foreach (Users user in Mesages)
+            {
+                if (user.Password == usernameImput)
+                {
+
+                }
+            }
+             
+
+            Console.ReadLine();
         }
     }
 }
--- Task 2/User.cs
+++ Task 2/User.cs
@@ -4,8 +4,25 @@ using System.Text;
 
 namespace Task_2
 {
-   public class User
+   public class Users
     {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string [] Mesages  { get; set; }
+
+        public Users(string aId, string aUsername, string aPassword, string[] aMesages)
+        {
+            Id = aId;
+            Username = aUsername;
+            Password = aPassword;
+            Mesages = aMesages;
+        }
+
+       
 
     }
+
+
+
 }
